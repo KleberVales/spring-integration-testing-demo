01af21731089fd810db34787b4b1f5b81f7a821e
Kleber Vales <klebervales.dev@gmail.com>
2025-08-13 12:22:15 -0300

    Implement Spring Boot with Kotlin DSL and JUnit 5

---
+++ src/main/java/com/example/springintegrationtestingdemo/SpringIntegrationTestingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintegrationtestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationTestingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationTestingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springintegrationtestingdemo/SpringIntegrationTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintegrationtestingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationTestingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
